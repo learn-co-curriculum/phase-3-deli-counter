184f492b4ea5057777ae41fdc481466cf858cd93
deniznida <denizunat@hotmail.com>
2015-08-10 15:31:14 -0400

    adds tutorial

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,27 @@
 # Write your code here.
 
-def take_a_number(deli, name)
-  deli << name
-  puts "Welcome, #{name}. You are number #{deli.length} in line."
-end
-
-def now_serving(deli)
-  if deli.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{deli.first}."
-    deli.shift
-  end
-end
-
-def line(deli)
-  if deli.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each_with_index do |person, i|
-      current_line << " #{i+1}. #{person}"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
     end
-
     puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
