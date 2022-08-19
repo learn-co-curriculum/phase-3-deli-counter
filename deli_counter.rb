28e6abab1e4278dd6d43456d3405dd093a41baf3
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-19 22:57:53 +0300

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def line deli
+
+if deli.empty?
+   puts "The line is currently empty."
+else
+    on_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      on_line << " #{i}. #{person}"
+    end
+    puts on_line
+  end
+end
+
+
+def take_a_number (deli, name)
+    deli.push name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+
+end
+
+
+
+def now_serving deli
+
+    if deli.empty?
+    puts "There is nobody waiting to be served!"
+
+    else
+        puts "Currently serving #{deli.first}."
+        deli.shift
+    end
+end
+
+
